52437827425c078d0884cccb99acdaa7d3b6b116
lumengde123 <lumengde123@gmail.com>
2024-04-15 09:33:41 +0800

    Site updated: 2024-04-15 09:33:41

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/dfa2.html","posts/ffab.html","posts/8fc.html","posts/8830.html","posts/4cbb.html","posts/c7fa.html","posts/361a.html","posts/478d.html","posts/d226.html","posts/41d2.html","posts/1ec0.html","posts/60a8.html","posts/ee37.html","posts/e449.html"];function toRandomPost(){
+var posts=["posts/ffab.html","posts/c7fa.html","posts/926.html","posts/64ae.html","posts/361a.html","posts/f4ed.html","posts/8fc.html","posts/478d.html","posts/ee37.html","posts/e449.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
